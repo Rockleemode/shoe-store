fb81c63503ebc3c0d678e67c96813cd602b7df4c
Rockleemode <96262404+Rockleemode@users.noreply.github.com>
2023-09-16 13:54:52 +0100

    added some interactivity in menu (you can click it now :) )

--- public/script.js
+++ public/script.js
@@ -1,6 +1,21 @@
 const icon = document.querySelector("#menu-icon");
 const menu = document.querySelector("#menu");
+const listItems = document.querySelectorAll("li");
+
 
 icon.addEventListener("click", ()=>{
     menu.classList.toggle("hidden");
 })
+
+listItems.forEach(list => {
+    list.addEventListener("click", () => {
+        // Remove border from all <li> elements
+        listItems.forEach(item => {
+            item.style.border = "none";
+        });
+
+        // Add a border to the clicked <li> element
+        list.style.border = "1px solid black";
+        list.style.borderRadius = "10px"
+    });
+});
